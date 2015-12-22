ded25bb3ee6ec36e82ee17215ca329aa79f834e0
David Ernst <git@dsernst.com>
2015-12-22 00:16:07 -0800

    Show done message on complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const stringify = require('csv-stringify')
 function writeCSV(filename, data) {
   stringify(data, {header: true}, (err, output) => {
     fs.writeFileSync(filename, output)
+    console.log('DONE: Wrote', filename)
   })
 }
 
