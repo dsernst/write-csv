31cbc455b51945dfdba77ad6ec373ed4590ea2d7
David Ernst <git@dsernst.com>
2015-12-22 00:17:30 -0800

    Throw the error while stringifying csv, if there is one

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const stringify = require('csv-stringify')
 
 function writeCSV(filename, data) {
   stringify(data, {header: true}, (err, output) => {
+    if (err) {
+      throw err
+    }
+
     fs.writeFileSync(filename, output)
     console.log('DONE: Wrote', filename)
   })
